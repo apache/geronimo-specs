2782e784f2f2f5e79dd6e54338ab58940d11b89c
Jeff Genender <jgenender@apache.org>
2006-02-09 07:01:21 +0000

    Adding Java Persistence Spec
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/branches/jee5_exp/geronimo-spec-jpa@376208 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/javax/persistence/EntityManager.java
@@ -0,0 +1,67 @@
+/**
+ *
+ * Copyright 2006 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+//
+// This source code implements specifications defined by the Java
+// Community Process. In order to remain compliant with the specification
+// DO NOT add / change / or delete method signatures!
+//
+package javax.persistence;
+
+public interface EntityManager {
+    
+    public void persist(Object entity); 
+    
+    public <T> T merge(T entity);
+    
+    public void remove(Object entity); 
+
+    public <T> T find(Class<T> entityClass, Object primaryKey); 
+    
+    public <T> T getReference(Class<T> entityClass, Object primaryKey); 
+    
+    public void flush();
+    
+    public void setFlushMode(FlushModeType flushMode);
+    
+    public FlushModeType getFlushMode(); 
+
+    // TODO: Comment this out as LockModeType is knowhere to be found in the spec
+    //public void lock(Object entity, LockModeType lockMode);
+    
+    public void refresh(Object entity); 
+    
+    public void clear();
+    
+    public boolean contains(Object entity);
+    
+    public Query createQuery(String ejbqlString);
+    
+    public Query createNamedQuery(String name); 
+    
+    public Query createNativeQuery(String sqlString);
+    
+    public Query createNativeQuery(String sqlString, Class resultClass);
+    
+    public Query createNativeQuery(String sqlString, String resultSetMapping);
+    
+    public void close();
+    
+    public boolean isOpen();
+    
+    public EntityTransaction getTransaction();
+}
