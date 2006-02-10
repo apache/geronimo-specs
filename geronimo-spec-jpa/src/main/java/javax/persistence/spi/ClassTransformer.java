0f9a692ff575d95057ac20c28e77f89e4f801539
Jeff Genender <jgenender@apache.org>
2006-02-10 21:13:19 +0000

    Add missing declarations
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/branches/jee5_exp@376859 13f79535-47bb-0310-9956-ffa450edef68

--- geronimo-spec-jpa/src/main/java/javax/persistence/spi/ClassTransformer.java
+++ geronimo-spec-jpa/src/main/java/javax/persistence/spi/ClassTransformer.java
@@ -18,10 +18,41 @@
 //
 // This source code implements specifications defined by the Java
 // Community Process. In order to remain compliant with the specification
-// DO NOT add / change / or delete method signatures!
+// DO NOT add/change/or delete method signatures!
 //
+
 package javax.persistence.spi;
 
+import java.security.ProtectionDomain;
+import java.lang.instrument.IllegalClassFormatException;
+
 public interface ClassTransformer {
+    /**
+     * Invoked when a class is being loaded or redefined. The implementation of this
+     * method may transform the supplied class file and return a new replacement class
+     * file.
+     *
+     * @param loader              The defining loader of the class to be transformed, may be null if
+     *                            the bootstrap loader
+     * @param className           The name of the class in the internal form of fully qualified
+     *                            class and interface names
+     * @param classBeingRedefined If this is a redefine, the class being redefined,
+     *                            otherwise null
+     * @param protectionDomain    The protection domain of the class being defined or
+     *                            redefined
+     * @param classfileBuffer     The input byte buffer in class file format - must not be
+     *                            modified
+     * @return A well-formed class file buffer (the result of the transform), or null if
+     *         no transform is performed
+     * @throws java.lang.instrument.IllegalClassFormatException
+     *          If the input does not represent a well-formed
+     *          class file
+     */
+    byte[] transform(
+            ClassLoader loader,
+            String className,
+            Class<?> classBeingRedefined,
+            ProtectionDomain protectionDomain,
+            byte[] classfileBuffer) throws IllegalClassFormatException;
 
 }
