3fca1a167f8da52aa6a69d83547345d904e3ed47
David Blevins <dblevins@apache.org>
2006-09-12 23:16:48 +0000

    GERONIMO-2358 Move java ee 5 specs into specs trunk
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk@442756 13f79535-47bb-0310-9956-ffa450edef68

---
+++ geronimo-jpa_3.0_spec/src/main/java/javax/persistence/NonUniqueResultException.java
@@ -0,0 +1,52 @@
+/**
+ *
+ * Copyright 2006 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+//
+// This source code implements specifications defined by the Java
+// Community Process. In order to remain compliant with the specification
+// DO NOT add / change / or delete method signatures!
+//
+package javax.persistence;
+
+
+/**
+ * @version $Revision$ $Date$
+ */
+public class NonUniqueResultException extends RuntimeException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 8841586522747620836L;
+
+    public NonUniqueResultException() {
+        super();
+    }
+
+    public NonUniqueResultException(String message) {
+        super(message);
+    }
+
+    public NonUniqueResultException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NonUniqueResultException(Throwable cause) {
+        super(cause);
+    }
+
+}
---
+++ geronimo-jpa_3.0_spec/src/main/java/javax/persistence/spi/PersistenceUnitInfo.java
@@ -0,0 +1,63 @@
+/**
+ *
+ * Copyright 2006 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+//
+// This source code implements specifications defined by the Java
+// Community Process. In order to remain compliant with the specification
+// DO NOT add / change / or delete method signatures!
+//
+package javax.persistence.spi;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+/**
+ * @version $Revision$ $Date$
+ */
+public interface PersistenceUnitInfo {
+    
+    public String getPersistenceUnitName();
+
+    public String getPersistenceProviderClassName();
+
+    public PersistenceUnitTransactionType getTransactionType();
+
+    public DataSource getJtaDataSource();
+
+    public DataSource getNonJtaDataSource();
+
+    public List<String> getMappingFileNames();
+
+    public List<URL> getJarFileUrls();
+
+    public URL getPersistenceUnitRootUrl();
+
+    public List<String> getManagedClassNames();
+
+    public boolean excludeUnlistedClasses();
+
+    public Properties getProperties();
+
+    public ClassLoader getClassLoader();
+
+    public void addTransformer(ClassTransformer transformer);
+
+    public ClassLoader getNewTempClassLoader();
+}
