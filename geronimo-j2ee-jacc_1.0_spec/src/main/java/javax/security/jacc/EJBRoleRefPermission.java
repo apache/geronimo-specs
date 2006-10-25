c3a82a7dfec5be3c53baf44efac2088a10690944
Jason Dillon <jdillon@apache.org>
2006-10-25 04:01:51 +0000

    Update license headers Drop empty resources folders
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk@467553 13f79535-47bb-0310-9956-ffa450edef68

--- geronimo-j2ee-jacc_1.0_spec/src/main/java/javax/security/jacc/EJBRoleRefPermission.java
+++ geronimo-j2ee-jacc_1.0_spec/src/main/java/javax/security/jacc/EJBRoleRefPermission.java
@@ -1,18 +1,20 @@
-/**
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Copyright 2003-2004 The Apache Software Foundation
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 //
@@ -37,6 +39,7 @@ import java.security.Permission;
  * The actions of an EJBRoleRefPermission identifies the role reference to
  * which the permission applies. An EJBRoleRefPermission is checked to
  * determine if the subject is a member of the role identified by the reference.
+ *
  * @version $Rev$ $Date$
  */
 public final class EJBRoleRefPermission extends Permission implements Serializable {
