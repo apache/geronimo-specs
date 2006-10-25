0e5196155a5c9737b82903ffdaf7ed4d4d423357
Jason Dillon <jdillon@apache.org>
2006-10-25 04:01:51 +0000

    Update license headers Drop empty resources folders
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk/geronimo-javamail_1.3.1_spec@467553 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/geronimo/mail/util/UUDecoderStream.java
+++ src/main/java/org/apache/geronimo/mail/util/UUDecoderStream.java
@@ -1,18 +1,20 @@
-/**
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Copyright 2003-2004 The Apache Software Foundation
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.apache.geronimo.mail.util;
@@ -29,6 +31,8 @@ import java.io.UnsupportedEncodingException;
  * decoding "on the fly" rather than decoding a single block of
  * data.  Since this version is intended for use by the MimeUtilty class,
  * it also handles line breaks in the encoded data.
+ *
+ * @version $Rev$ $Date$
  */
 public class UUDecoderStream extends FilterInputStream {
     // maximum number of chars that can appear in a single line
