a7e3103fd8ef9ea6417a8371280be228e861d5b6
Jason Dillon <jdillon@apache.org>
2006-10-25 04:01:51 +0000

    Update license headers Drop empty resources folders
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk/geronimo-servlet_2.5_spec@467553 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/javax/servlet/http/HttpSessionBindingEvent.java
+++ src/main/java/javax/servlet/http/HttpSessionBindingEvent.java
@@ -1,25 +1,25 @@
 /*
-* Copyright 2004 The Apache Software Foundation
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 package javax.servlet.http;
 
-
-
 /**
- *
  * Events of this type are either sent to an object that implements
  * {@link HttpSessionBindingListener} when it is bound or 
  * unbound from a session, or to a {@link HttpSessionAttributeListener} 
@@ -39,12 +39,7 @@ package javax.servlet.http;
  * @see 		HttpSessionBindingListener
  * @see			HttpSessionAttributeListener
  */
-
 public class HttpSessionBindingEvent extends HttpSessionEvent {
-
-
-
-
     /* The name to which the object is being bound or unbound */
 
     private String name;
