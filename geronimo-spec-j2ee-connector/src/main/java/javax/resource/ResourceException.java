bced36e1279d74901db5ae617f3b6c3ddae7fb05
Vamsavardhana Chillakuru <vamsic007@apache.org>
2006-12-09 08:33:52 +0000

    GERONIMO-1519 ResourceException.toString() can return null   o The method seems to have been introduced to account for a bad testcase.  Removed ResourceException.toString() and the corresponding test ResourceExceptionTest.testToString()   o For a discussion on this, see http://www.mail-archive.com/dev%40geronimo.apache.org/msg38005.html
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/branches/1_1@484948 13f79535-47bb-0310-9956-ffa450edef68

--- geronimo-spec-j2ee-connector/src/main/java/javax/resource/ResourceException.java
+++ geronimo-spec-j2ee-connector/src/main/java/javax/resource/ResourceException.java
@@ -76,11 +76,4 @@ public class ResourceException extends Exception {
         // unit tests revealed that Throwable.initCause is not invoked
         this.linkedException = ex;
     }
-
-    public String toString() {
-        // unit tests revealed that the errorCode is not included
-        String className = getClass().getName();
-        String msg = getMessage();
-        return msg != null ? className + ": "+msg : className;
-    }
 }
