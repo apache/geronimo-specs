ce4a668f4383b9d17ef6d0736b13d39358642969
David Blevins <dblevins@apache.org>
2006-12-21 03:20:05 +0000

    Fixed class defs so this is now 100% compliant with Annotations 1.0
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk/geronimo-annotation_1.0_spec@489255 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/javax/annotation/Generated.java
+++ src/main/java/javax/annotation/Generated.java
@@ -24,22 +24,32 @@
 //
 package javax.annotation;
 
-import java.lang.annotation.Target;
-import java.lang.annotation.Retention;
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * @version $Rev$ $Date$
  */
-@Target({ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.FIELD, 
-ElementType.LOCAL_VARIABLE, ElementType.METHOD, ElementType.PACKAGE, 
-ElementType.PARAMETER, ElementType.TYPE})
+@Target({
+        ElementType.PACKAGE,
+        ElementType.TYPE,
+        ElementType.ANNOTATION_TYPE,
+        ElementType.METHOD,
+        ElementType.CONSTRUCTOR,
+        ElementType.FIELD,
+        ElementType.LOCAL_VARIABLE,
+        ElementType.PARAMETER
+        })
 
 @Retention(RetentionPolicy.SOURCE)
-
+@Documented
 public @interface Generated {
     String[] value();
+
     String date() default "";
+
     String comments() default "";
 }
