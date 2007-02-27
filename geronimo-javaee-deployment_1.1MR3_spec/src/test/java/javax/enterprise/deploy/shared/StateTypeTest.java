6e0c5930c9280a0b950ec6db4df79538d557aa53
David Jencks <djencks@apache.org>
2007-02-27 23:07:43 +0000

    GERONIMO-2701 update enterprise deployment spec to 1.1MR3
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk@512474 13f79535-47bb-0310-9956-ffa450edef68

---
+++ geronimo-javaee-deployment_1.1MR3_spec/src/test/java/javax/enterprise/deploy/shared/StateTypeTest.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+//
+// This source code implements specifications defined by the Java
+// Community Process. In order to remain compliant with the specification
+// DO NOT add / change / or delete method signatures!
+//
+
+package javax.enterprise.deploy.shared;
+
+import junit.framework.TestCase;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class StateTypeTest extends TestCase {
+    public void testValues() {
+        assertEquals(0, StateType.RUNNING.getValue());
+        assertEquals(1, StateType.COMPLETED.getValue());
+        assertEquals(2, StateType.FAILED.getValue());
+        assertEquals(3, StateType.RELEASED.getValue());
+    }
+
+    public void testToString() {
+        assertEquals("running", StateType.RUNNING.toString());
+        assertEquals("completed", StateType.COMPLETED.toString());
+        assertEquals("failed", StateType.FAILED.toString());
+        assertEquals("released", StateType.RELEASED.toString());
+        // only possible due to package local access
+        assertEquals("5", new ActionType(5).toString());
+    }
+
+    public void testValueToSmall() {
+        try {
+            StateType.getStateType(-1);
+            fail("Expected AIOOBE");
+        } catch (ArrayIndexOutOfBoundsException aioobe) {
+        }
+    }
+
+    public void testValueToLarge() {
+        try {
+            StateType.getStateType(5);
+            fail("Expected AIOOBE");
+        } catch (ArrayIndexOutOfBoundsException aioobe) {
+        }
+    }
+}
