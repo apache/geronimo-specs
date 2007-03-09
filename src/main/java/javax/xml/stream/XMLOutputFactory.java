d716052b4387a632ecced3420a20b6bf191ace95
Matt Richard Hogstrom <hogstrom@apache.org>
2007-03-09 18:31:59 +0000

    Updated the mechanism to locate Factory classes as well as fixed the issues on XMLOutputFactory with regard to the problems with the spec.  See JSR172 MR-1 for more information
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk/geronimo-stax-api_1.0_spec@516497 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/javax/xml/stream/XMLOutputFactory.java
+++ src/main/java/javax/xml/stream/XMLOutputFactory.java
@@ -1,3 +1,22 @@
+/*
+ **
+ ** Licensed to the Apache Software Foundation (ASF) under one
+ ** or more contributor license agreements.  See the NOTICE file
+ ** distributed with this work for additional information
+ ** regarding copyright ownership.  The ASF licenses this file
+ ** to you under the Apache License, Version 2.0 (the
+ ** "License"); you may not use this file except in compliance
+ ** with the License.  You may obtain a copy of the License at
+ **
+ **  http://www.apache.org/licenses/LICENSE-2.0
+ **
+ ** Unless required by applicable law or agreed to in writing,
+ ** software distributed under the License is distributed on an
+ ** "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ ** KIND, either express or implied.  See the License for the
+ ** specific language governing permissions and limitations
+ ** under the License.
+ */
 package javax.xml.stream;
 
 public abstract class XMLOutputFactory {
@@ -7,13 +26,13 @@ public abstract class XMLOutputFactory {
 			throws FactoryConfigurationError {
 		return (XMLOutputFactory) FactoryLocator.locate(
 				"javax.xml.stream.XMLOutputFactory",
-				"com.bea.xml.stream.MXParserFactory");
+				"com.bea.xml.stream.XMLOutputFactoryBase");
 	}
 
-	public static XMLOutputFactory newInstance(String factoryId,
+	public static XMLInputFactory newInstance(String factoryId,
 			java.lang.ClassLoader classLoader) throws FactoryConfigurationError {
-		return (XMLOutputFactory) FactoryLocator.locate(factoryId,
-				"com.bea.xml.stream.MXParserFactory", classLoader);
+		return (XMLInputFactory) FactoryLocator.locate(factoryId,
+				"com.bea.xml.stream.XMLInputFactoryBase", classLoader);
 	}
 
 	public abstract XMLStreamWriter createXMLStreamWriter(java.io.Writer stream)
