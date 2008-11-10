752d5edf7cc927b00f4df69143b20b14fbb28013
Jarek Gawor <gawor@apache.org>
2008-11-10 01:50:31 +0000

    support jaxb 1.0 createContext() method and fix TypeConstraintException issues
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk/geronimo-jaxb_2.1_spec@712595 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/javax/xml/bind/TypeConstraintException.java
+++ src/main/java/javax/xml/bind/TypeConstraintException.java
@@ -17,6 +17,7 @@
 package javax.xml.bind;
 
 import java.io.PrintStream;
+import java.io.PrintWriter;
 
 public class TypeConstraintException extends RuntimeException {
 
@@ -63,12 +64,39 @@ public class TypeConstraintException extends RuntimeException {
                 super.toString();
     }
 
-    public void printStackTrace() {
-        super.printStackTrace();
+    @Override
+    public Throwable getCause() {
+        return linkedException;
     }
 
-    public void printStackTrace(PrintStream ps) {
-        super.printStackTrace(ps);
+    @Override
+    public void printStackTrace(PrintStream s) {
+        synchronized (s) {
+            s.println(this);
+            StackTraceElement[] trace = getStackTrace();
+            for (int i=0; i < trace.length; i++) {
+                s.println("\tat " + trace[i]);
+            }
+            Throwable ourCause = getCause();
+            if (ourCause != null) {
+                ourCause.printStackTrace(s);
+            }
+        }
+    }
+
+    @Override
+    public void printStackTrace(PrintWriter s) {
+        synchronized (s) {
+            s.println(this);
+            StackTraceElement[] trace = getStackTrace();
+            for (int i=0; i < trace.length; i++) {
+                s.println("\tat " + trace[i]);
+            }
+            Throwable ourCause = getCause();
+            if (ourCause != null) {
+                ourCause.printStackTrace(s);
+            }
+        }
     }
 
 }
