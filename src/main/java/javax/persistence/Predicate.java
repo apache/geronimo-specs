79576e8a36c77f44f87229d7c8ab342ca2993522
David Jencks <djencks@apache.org>
2008-12-04 08:42:18 +0000

    GERONIMO-4410 apply patch from Jeremy Bauer that removes javadoc mistakenly included as it was copied from Sun sourced documentation
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk/geronimo-jpa_2.0_spec@723243 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/javax/persistence/Predicate.java
+++ src/main/java/javax/persistence/Predicate.java
@@ -24,35 +24,11 @@
 //
 package javax.persistence;
 
-/**
- * Interface used to define compound predicates.
- */
 public interface Predicate {
-    /**
-     * Creates an AND of the predicate with the argument.
-     *
-     * @param predicate -
-     *                  A simple or compound predicate
-     * @return the predicate that is the AND of the original simple or compound
-     *         predicate and the argument.
-     */
+
     Predicate and(Predicate predicate);
 
-    /**
-     * Creates an OR of the predicate with the argument.
-     *
-     * @param predicate -
-     *                  A simple or compound predicate
-     * @return the predicate that is the OR of the original simple or compound
-     *         predicate and the argument.
-     */
     Predicate or(Predicate predicate);
 
-    /**
-     * Creates a negation of the predicate with the argument.
-     *
-     * @return the predicate that is the negation of the original simple or
-	 *         compound predicate.
-	 */
 	Predicate not();
 }
--- src/main/java/javax/persistence/SelectItem.java
+++ src/main/java/javax/persistence/SelectItem.java
@@ -24,27 +24,9 @@
 //
 package javax.persistence;
 
-/**
- * SelectItem instances are used in specifying the query's select list.
- * <p/>
- * The methods of this interface are used to define arguments that can be passed
- * to the orderBy method for use in ordering selected items of the query result.
- */
 public interface SelectItem extends OrderByItem {
-    /**
-     * Return an OrderByItem referencing the SelectItem and specifying ascending
-     * ordering. The SelectItem must correspond to an orderable value.
-     *
-     * @return order-by item
-     */
+
     OrderByItem asc();
 
-    /**
-     * Return an OrderByItem referencing the SelectItem and specifying
-     * descending ordering. The SelectItem must correspond to an orderable
-     * value.
-     *
-     * @return order-by item
-     */
     OrderByItem desc();
 }
