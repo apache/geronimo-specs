f197e0bff31f0987f3c2bd1ae20fdb5cfc27a91b
Donald Woods <dwoods@apache.org>
2009-04-22 21:16:58 +0000

    GERONIMO-4619 JSR-303 Bean Validation API (no Impl) based on the 1.0 PFD.  Note: Still need to finish the Validation.java code, but the APIs are ready to start using in OpenJPA.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk/geronimo-validation_1.0_spec@767665 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/javax/validation/constraints/Min.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package javax.validation.constraints;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * @version $Rev$ $Date$
+ */
+@Target( { FIELD, METHOD, ANNOTATION_TYPE })
+@Retention(RUNTIME)
+@Documented
+public @interface Min {
+
+    /**
+     * Min[]
+     */
+    @Target( { FIELD, METHOD, ANNOTATION_TYPE })
+    @Retention(RUNTIME)
+    @Documented
+    @interface List {
+        Min[] value();
+    }
+
+    /**
+     * @return Class[]
+     */
+    Class<?>[] groups() default {};
+
+    /**
+     * Default message interpolation
+     * 
+     * @return String
+     */
+    String message() default "{constraint.min}";
+
+    /**
+     * @return long
+     */
+    long value();
+}
