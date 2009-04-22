d4dac343456f3a1cc2e4471e4062cc6194b6516d
Donald Woods <dwoods@apache.org>
2009-04-22 21:16:58 +0000

    GERONIMO-4619 JSR-303 Bean Validation API (no Impl) based on the 1.0 PFD.  Note: Still need to finish the Validation.java code, but the APIs are ready to start using in OpenJPA.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk@767665 13f79535-47bb-0310-9956-ffa450edef68

---
+++ geronimo-validation_1.0_spec/src/main/java/javax/validation/Validation.java
@@ -0,0 +1,192 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package javax.validation;
+
+import java.util.List;
+
+import javax.validation.bootstrap.GenericBootstrap;
+import javax.validation.bootstrap.ProviderSpecificBootstrap;
+import javax.validation.spi.BootstrapState;
+import javax.validation.spi.ValidationProvider;
+
+/**
+ * Note: From Section 4.4.5 Validation of the 1.0 PFD Spec - Validation
+ * implementations may only provide the following three public static methods:
+ * buildDefaultValidatorFactory(), byDefaultProvider(), byProvider()
+ * 
+ * @version $Rev$ $Date$
+ */
+public class Validation {
+
+    /**
+     * @return ValidatorFactory
+     * @throws ValidationException
+     */
+    public static ValidatorFactory buildDefaultValidatorFactory() {
+        return byDefaultProvider().configure().buildValidatorFactory();
+    }
+
+    /**
+     * @return GenericBootstrap
+     */
+    public static GenericBootstrap byDefaultProvider() {
+        return new GenericBootstrapImpl();
+    }
+
+    /**
+     * @param configurationType
+     * @return ProviderSpecificBootstrap<T>
+     */
+    public static <T extends Configuration<T>> ProviderSpecificBootstrap<T> byProvider(
+        Class<T> configurationType) {
+        return new ProviderSpecificBootstrapImpl<T>(configurationType);
+    }
+
+    /*
+     * (non-Javadoc) See Section 4.4.5 Validation - Must be private
+     */
+    private static class DefaultValidationProviderResolver implements
+        ValidationProviderResolver {
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see
+         * javax.validation.ValidationProviderResolver#getValidationProviders()
+         */
+        // FIXME JSR-303 - Needs to be implemented
+        public List<ValidationProvider> getValidationProviders() {
+            throw new ValidationException("TODO - Not implemented yet");
+        }
+    }
+
+    /*
+     * (non-Javadoc) See Section 4.4.5 Validation - Must be private
+     */
+    private static class GenericBootstrapImpl implements GenericBootstrap,
+        BootstrapState {
+
+        private ValidationProviderResolver vpResolver;
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see javax.validation.bootstrap.GenericBootstrap#configure()
+         */
+        public Configuration<?> configure() {
+            ValidationProviderResolver resolv = vpResolver;
+            try {
+                if (resolv == null)
+                    resolv = new DefaultValidationProviderResolver();
+                return resolv.getValidationProviders().get(0)
+                    .createGenericConfiguration(this);
+            } catch (Exception e) {
+                throw new ValidationException(
+                    "Could not create configuration.  Nested exception = " +
+                        e.getMessage());
+            }
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see
+         * javax.validation.spi.BootstrapState#getValidationProviderResolver()
+         */
+        public ValidationProviderResolver getValidationProviderResolver() {
+            return vpResolver;
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see
+         * javax.validation.bootstrap.GenericBootstrap#providerResolver(javax
+         * .validation.ValidationProviderResolver)
+         */
+        public GenericBootstrap providerResolver(
+            ValidationProviderResolver resolver) {
+            vpResolver = resolver;
+            return this;
+        }
+    }
+
+    /*
+     * (non-Javadoc) See Section 4.4.5 Validation - Must be private
+     */
+    private static class ProviderSpecificBootstrapImpl<T extends Configuration<T>>
+        implements ProviderSpecificBootstrap<T> {
+
+        private Class<T> cfgType = null;
+        private ValidationProviderResolver vpResolver = null;
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @seejavax.validation.bootstrap.ProviderSpecificBootstrap#
+         * ProviderSpecificBootstrap(Class<T>)
+         */
+        public ProviderSpecificBootstrapImpl(Class<T> configurationType) {
+            cfgType = configurationType;
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see javax.validation.bootstrap.ProviderSpecificBootstrap#configure()
+         */
+        public T configure() {
+            T cfg = null;
+
+            if (cfgType == null)
+                throw new ValidationException(
+                    "No configuration type was supplied");
+            if (vpResolver == null)
+                vpResolver = new DefaultValidationProviderResolver();
+
+            for (ValidationProvider vProvider : vpResolver
+                .getValidationProviders()) {
+                if (vProvider.isSuitable(cfgType)) {
+                    GenericBootstrapImpl bootstrap = new GenericBootstrapImpl();
+                    bootstrap.providerResolver(vpResolver);
+                    // FIXME JSR-303 - Need to create a Configuration<T>
+                    // from the above bootstrap and configurationType
+
+                }
+            }
+
+            if (cfg != null)
+                return cfg;
+            else
+                throw new ValidationException("No provider found for " +
+                    "configuration type " + cfgType);
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see
+         * javax.validation.bootstrap.ProviderSpecificBootstrap#providerResolver
+         * (javax.validation.ValidationProviderResolver)
+         */
+        public ProviderSpecificBootstrap<T> providerResolver(
+            ValidationProviderResolver resolver) {
+            vpResolver = resolver;
+            return this;
+        }
+    }
+}
