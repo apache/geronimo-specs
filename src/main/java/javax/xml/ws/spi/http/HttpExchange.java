4985e3633fd1dbb1f3f0f5c53612d3cdbe9860d9
Daniel Kulp <dkulp@apache.org>
2009-06-13 01:38:53 +0000

    Updates to make it fully compat (according to japi)
    git-svn-id: https://svn.apache.org/repos/asf/cxf/sandbox/geronimo-jaxws_2.2_spec@784327 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/javax/xml/ws/spi/http/HttpExchange.java
+++ src/main/java/javax/xml/ws/spi/http/HttpExchange.java
@@ -19,63 +19,71 @@
 
 package javax.xml.ws.spi.http;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.security.Principal;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * @since 2.2
  */
 public abstract class HttpExchange {
 
-            public HttpExchange()
-            {
-            }
-
-            public abstract Map getRequestHeaders();
-
-            public abstract String getRequestHeader(String name);
-
-            public abstract Map getResponseHeaders();
-
-            public abstract void addResponseHeader(String name, String value);
-
-            public abstract String getRequestURI();
-
-            public abstract String getContextPath();
-
-            public abstract String getRequestMethod();
-
-            public abstract HttpContext getHttpContext();
-
-            public abstract void close()
-                throws IOException;
-
-            public abstract InputStream getRequestBody()
-                throws IOException;
-
-            public abstract OutputStream getResponseBody()
-                throws IOException;
-
-            public abstract void setStatus(int status);
-
-            public abstract InetSocketAddress getRemoteAddress();
-
-            public abstract InetSocketAddress getLocalAddress();
-
-            public abstract String getProtocol();
-
-            public abstract String getScheme();
-
-            public abstract String getPathInfo();
-
-            public abstract String getQueryString();
-
-            public abstract Object getAttribute(String name);
-
-            public abstract Set getAttributeNames();
-
-            public abstract Principal getUserPrincipal();
-
-            public abstract boolean isUserInRole(String role);
-
-            public static final String REQUEST_CIPHER_SUITE = "javax.xml.ws.spi.http.request.cipher.suite";
-            public static final String REQUEST_KEY_SIZE = "javax.xml.ws.spi.http.request.key.size";
-            public static final String REQUEST_X509CERTIFICATE = "javax.xml.ws.spi.http.request.cert.X509Certificate";
+    public HttpExchange() {
+    }
+    
+    public abstract Map<String, List<String>> getRequestHeaders();
+    
+    public abstract String getRequestHeader(String name);
+    
+    public abstract Map<String, List<String>> getResponseHeaders();
+    
+    public abstract void addResponseHeader(String name, String value);
+    
+    public abstract String getRequestURI();
+    
+    public abstract String getContextPath();
+    
+    public abstract String getRequestMethod();
+    
+    public abstract HttpContext getHttpContext();
+    
+    public abstract void close()
+        throws IOException;
+    
+    public abstract InputStream getRequestBody()
+        throws IOException;
+    
+    public abstract OutputStream getResponseBody()
+        throws IOException;
+    
+    public abstract void setStatus(int status);
+    
+    public abstract InetSocketAddress getRemoteAddress();
+    
+    public abstract InetSocketAddress getLocalAddress();
+    
+    public abstract String getProtocol();
+    
+    public abstract String getScheme();
+    
+    public abstract String getPathInfo();
+    
+    public abstract String getQueryString();
+    
+    public abstract Object getAttribute(String name);
+    
+    public abstract Set<String> getAttributeNames();
+    
+    public abstract Principal getUserPrincipal();
+    
+    public abstract boolean isUserInRole(String role);
+    
+    public static final String REQUEST_CIPHER_SUITE = "javax.xml.ws.spi.http.request.cipher.suite";
+    public static final String REQUEST_KEY_SIZE = "javax.xml.ws.spi.http.request.key.size";
+    public static final String REQUEST_X509CERTIFICATE = "javax.xml.ws.spi.http.request.cert.X509Certificate";
 }
