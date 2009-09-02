b8a6c4f62341e0b6ccd28fb71c518c429a0028ec
Donald Woods <dwoods@apache.org>
2009-09-02 17:00:37 +0000

    GERONIMO-4619 Minor update for 1.0 CR4 api level
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk/geronimo-validation_1.0_spec@810610 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/javax/validation/Constraint.java
+++ src/main/java/javax/validation/Constraint.java
@@ -21,6 +21,7 @@ import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import java.lang.annotation.Retention;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Target;
+import java.lang.annotation.Annotation;
 
 /**
  * @version $Rev$ $Date$
