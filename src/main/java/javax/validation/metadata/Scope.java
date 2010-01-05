7aab44310454585cefd781920f62a0d4042e1980
Kevan Lee Miller <kevan@apache.org>
2010-01-05 15:48:21 +0000

    GERONIMO-5014 Updates to validator API. Patch from Mark Struberg. Many thanks Mark!
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk/geronimo-validation_1.0_spec@896104 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/javax/validation/metadata/Scope.java
+++ src/main/java/javax/validation/metadata/Scope.java
@@ -16,31 +16,7 @@
  */
 package javax.validation.metadata;
 
-import java.lang.annotation.ElementType;
-import java.util.Set;
-
-/**
- * @version $Rev$ $Date$
- */
-public interface ElementDescriptor {
-
-    boolean hasConstraints();
-
-    Class<?> getElementClass();
-
-    Set<ConstraintDescriptor<?>> getConstraintDescriptors();
-
-    ConstraintFinder findConstraints();
-
-    interface ConstraintFinder {
-        ConstraintFinder unorderedAndMatchingGroups(Class<?>... groups);
-
-        ConstraintFinder lookingAt(Scope scope);
-
-        ConstraintFinder declaredOn(ElementType... types);
-
-        Set<ConstraintDescriptor<?>> getConstraintDescriptors();
-
-        boolean hasConstraints();
-    }
+public enum Scope {
+    LOCAL_ELEMENT,
+    HIERARCHY
 }
