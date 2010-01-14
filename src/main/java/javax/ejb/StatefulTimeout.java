fd883f13187fafe67a078d55bf42965d7f67b9f6
David Blevins <dblevins@apache.org>
2010-01-14 16:19:07 +0000

    Fixed Incompatibilities based on the 24-Nov-2009 Java EE 6 TCK Still in progress
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk/geronimo-ejb_3.1_spec@899269 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/javax/ejb/StatefulTimeout.java
+++ src/main/java/javax/ejb/StatefulTimeout.java
@@ -25,10 +25,9 @@ import java.util.concurrent.TimeUnit;
 import java.lang.annotation.Target;
 import java.lang.annotation.Retention;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
-import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 
-@Target(value = {METHOD, TYPE})
+@Target(value = {TYPE})
 @Retention(value = RUNTIME)
 public @interface StatefulTimeout {
     long value();
