f9b516dd1f267c6459cff14ac1a8d83ffdcaf192
David Blevins <dblevins@apache.org>
2010-01-14 20:12:38 +0000

    Added classes as required by the 24-Nov-2009 Java EE 6 TCK Methods of AbstractProcessor and Completions implemented with 'throw new UnsupportedOperationException()'
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk/geronimo-annotation_1.1_spec@899387 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/javax/annotation/processing/Messager.java
@@ -0,0 +1,42 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+//
+// This source code implements specifications defined by the Java
+// Community Process. In order to remain compliant with the specification
+// DO NOT add / change / or delete method signatures!
+//
+package javax.annotation.processing;
+
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.AnnotationValue;
+import javax.lang.model.element.Element;
+import javax.tools.Diagnostic.Kind;
+
+/**
+ * @version $Revision$ $Date$
+ */
+public interface Messager {
+
+    void printMessage(Kind kind, CharSequence charSequence);
+
+    void printMessage(Kind kind, CharSequence charSequence, Element element);
+
+    void printMessage(Kind kind, CharSequence charSequence, Element element, AnnotationMirror annotationMirror);
+
+    void printMessage(Kind kind, CharSequence charSequence, Element element, AnnotationMirror annotationMirror, AnnotationValue annotationValue);
+
+}
