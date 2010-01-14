70b16ca3f2156c27825edfdb1072039a17afadca
David Blevins <dblevins@apache.org>
2010-01-14 16:19:07 +0000

    Fixed Incompatibilities based on the 24-Nov-2009 Java EE 6 TCK Still in progress
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk@899269 13f79535-47bb-0310-9956-ffa450edef68

--- geronimo-ejb_3.1_spec/src/main/java/javax/ejb/Lock.java
+++ geronimo-ejb_3.1_spec/src/main/java/javax/ejb/Lock.java
@@ -26,7 +26,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.ElementType.METHOD;
 
-@java.lang.annotation.Target(value = {METHOD, TYPE})
+@java.lang.annotation.Target(value = {TYPE, METHOD})
 @java.lang.annotation.Retention(value = RUNTIME)
 public @interface Lock {
     javax.ejb.LockType value() default WRITE;
