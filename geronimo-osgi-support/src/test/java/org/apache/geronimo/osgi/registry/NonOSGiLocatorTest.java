8a8fe6d2c82c5f1cb23d47b825f641a51bf6b791
Richard McGuire <rickmcguire@apache.org>
2010-03-04 15:16:48 +0000

    GERONIMO-5133 Geroinimo versions of the specs should pick up the additions made to the service mix versions of the bundles.
    Base OSGi support bundle plus changes to the JPA spec to use the new locator mechanism. 
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk@919017 13f79535-47bb-0310-9956-ffa450edef68

---
+++ geronimo-osgi-support/src/test/java/org/apache/geronimo/osgi/registry/NonOSGiLocatorTest.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.geronimo.osgi.registry;
+
+import java.io.InputStream;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+import org.apache.geronimo.osgi.locator.ProviderLocator;
+
+public class NonOSGiLocatorTest {
+    @Test
+    public void testLocator() throws Exception {
+        // Run outside of an OSGi framework, this should just return null without
+        // causing an error
+        Class<?> target = ProviderLocator.locate("org.apache.geronimo.osgi.registry.TestTarget");
+        assertNull(target);
+    }
+}
