3ee321f09a9d5754e9f6f7271b7b319dd5295a5e
Richard McGuire <rickmcguire@apache.org>
2010-03-08 18:40:51 +0000

    GERONIMO-5133 Geroinimo versions of the specs should pick up the additions made to the service mix versions of the bundles.
    javamail spec changes. 
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk@920441 13f79535-47bb-0310-9956-ffa450edef68

---
+++ geronimo-javamail_1.4_spec/src/main/java/org/apache/geronimo/mail/MailProviderBundleTrackerCustomizer.java
@@ -0,0 +1,80 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.geronimo.mail;
+
+import java.net.URL;
+
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleEvent;
+import org.osgi.service.log.LogService;
+import org.osgi.util.tracker.BundleTrackerCustomizer;
+
+public class MailProviderBundleTrackerCustomizer implements BundleTrackerCustomizer {
+    // our base Activator (used as a service source)
+    private Activator activator;
+    // the bundle hosting the activation code
+    private Bundle activationBundle;
+
+    public MailProviderBundleTrackerCustomizer(Activator a, Bundle b) {
+        activator = a;
+        activationBundle = b;
+    }
+
+    /**
+     * Handle the activation of a new bundle.
+     *
+     * @param bundle The source bundle.
+     * @param event  The bundle event information.
+     *
+     * @return A return object.
+     */
+    @Override
+    public Object addingBundle(Bundle bundle, BundleEvent event) {
+        log(LogService.LOG_DEBUG, "Bundle Considered for mailcap providers: " + bundle.getSymbolicName());
+        if (bundle.equals(activationBundle)) {
+            return null;
+        }
+
+        return MailProviderRegistry.registerBundle(bundle);
+    }
+
+
+    @Override
+    public void modifiedBundle(Bundle bundle, BundleEvent event, Object object) {
+        log(LogService.LOG_DEBUG, "Bundle Considered for mailcap providers: " + bundle.getSymbolicName());
+        // this will update for the new bundle
+        MailProviderRegistry.registerBundle(bundle);
+    }
+
+    @Override
+    public void removedBundle(Bundle bundle, BundleEvent event, Object object) {
+        MailProviderRegistry.unregisterBundle(bundle);
+    }
+
+    private void log(int level, String message) {
+        activator.log(level, message);
+    }
+
+    private void log(int level, String message, Throwable th) {
+        activator.log(level, message, th);
+    }
+}
