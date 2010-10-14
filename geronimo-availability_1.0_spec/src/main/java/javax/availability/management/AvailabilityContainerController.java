c74110a2f469799400093bc5c457ae2adb24777a
Alan Cabrera <adc@apache.org>
2010-10-14 18:27:56 +0000

    GERONIMO-5645 start from older version
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk@1022640 13f79535-47bb-0310-9956-ffa450edef68

---
+++ geronimo-availability_1.0_spec/src/main/java/javax/availability/management/AvailabilityContainerController.java
@@ -0,0 +1,36 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package javax.availability.management;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public interface AvailabilityContainerController extends AvailabilityUnitController {
+
+    void init(String name, AvailabilityAgentService service) throws AvailabilityException;
+
+    void activate(ActivationReason reason) throws AvailabilityException;
+
+    void deactivate(DeactivationReason reason) throws AvailabilityException;
+
+    void terminate() throws AvailabilityException;
+
+    void checkHealth() throws AvailabilityException;
+
+    AvailabilityUnitController instantiateAvailabilityUnit(String name, AvailabilityAgentService service) throws AvailabilityException;
+}
