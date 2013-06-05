afb25e1c9085d09743f02c3ad271d6e55fb49648
Haihong Xu <xuhaihong@apache.org>
2013-06-05 01:45:53 +0000

    GERONIMO-6471 IllegalStateException : Cause already initialized is thrown while mail provider failed to connect or sent the messages
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk/geronimo-javamail_1.4_spec@1489683 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/javax/mail/MessagingExceptionTest.java
+++ src/test/java/javax/mail/MessagingExceptionTest.java
@@ -25,29 +25,51 @@ import junit.framework.TestCase;
  * @version $Revision $ $Date$
  */
 public class MessagingExceptionTest extends TestCase {
-    private RuntimeException d;
+    private RuntimeException e;
+    private MessagingException d;
     private MessagingException c;
     private MessagingException b;
     private MessagingException a;
     public MessagingExceptionTest(String name) {
         super(name);
     }
+    
     protected void setUp() throws Exception {
         super.setUp();
-        a = new MessagingException("A");
+        
+        //Initialize cause with null, make sure the getCause will not be affected
+        a = new MessagingException("A", null);
         b = new MessagingException("B");
         c = new MessagingException("C");
-        d = new RuntimeException("D");
+        d = new MessagingException("D");
+        e = new RuntimeException("E");
     }
+    
     public void testMessagingExceptionString() {
         assertEquals("A", a.getMessage());
     }
+    
     public void testNextException() {
         assertTrue(a.setNextException(b));
         assertEquals(b, a.getNextException());
+        assertEquals(b, a.getCause());
+        
         assertTrue(a.setNextException(c));
         assertEquals(b, a.getNextException());
         assertEquals(c, b.getNextException());
+        assertEquals(c, b.getCause());
+        
+        assertTrue(a.setNextException(d));
+        
+        assertEquals(b, a.getNextException());
+        assertEquals(b, a.getCause());
+        
+        assertEquals(c, b.getNextException());
+        assertEquals(c, b.getCause());
+        
+        assertEquals(d, c.getNextException());
+        assertEquals(d, c.getCause());
+        
         String message = a.getMessage();
         int ap = message.indexOf("A");
         int bp = message.indexOf("B");
@@ -56,14 +78,17 @@ public class MessagingExceptionTest extends TestCase {
         assertTrue("B does not contain 'B'", bp != -1);
         assertTrue("C does not contain 'C'", cp != -1);
     }
+    
     public void testNextExceptionWrong() {
-        assertTrue(a.setNextException(d));
+        assertTrue(a.setNextException(e));
         assertFalse(a.setNextException(b));
     }
+    
     public void testNextExceptionWrong2() {
-        assertTrue(a.setNextException(d));
+        assertTrue(a.setNextException(e));
         assertFalse(a.setNextException(b));
     }
+    
     public void testMessagingExceptionStringException() {
         MessagingException x = new MessagingException("X", a);
         assertEquals("X (javax.mail.MessagingException: A)", x.getMessage());
