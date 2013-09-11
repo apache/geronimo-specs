c04061482e25a43a271ef9aaad07c14220446026
Romain Manni-Bucau <rmannibucau@apache.org>
2013-09-11 13:21:06 +0000

    GERONIMO-6489 adding jbatch API
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk/geronimo-jbatch_1.0_spec@1521825 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/javax/batch/api/chunk/listener/AbstractItemProcessListener.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package javax.batch.api.chunk.listener;
+
+public abstract class AbstractItemProcessListener implements ItemProcessListener {
+    @Override
+    public void beforeProcess(final Object item) throws Exception {
+        // no-op
+    }
+
+    @Override
+    public void afterProcess(final Object item, final Object result) throws Exception {
+        // no-op
+    }
+
+    @Override
+    public void onProcessError(final Object item, final Exception ex) throws Exception {
+        // no-op
+    }
+}
