6e33af6821fe93c258b9d2504ab7bdb9d6230be4
Mark Struberg <struberg@apache.org>
2013-11-22 07:47:01 +0000

    GERONIMO-6458 remove java7 Exception cts
    As per spec those Exceptions do only implement the java5 Exception constructors.
    Allows for compiling with java6.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk@1544426 13f79535-47bb-0310-9956-ffa450edef68

--- geronimo-jcdi_1.1_spec/src/main/java/javax/enterprise/inject/spi/DefinitionException.java
+++ geronimo-jcdi_1.1_spec/src/main/java/javax/enterprise/inject/spi/DefinitionException.java
@@ -26,22 +26,23 @@ package javax.enterprise.inject.spi;
  */
 public class DefinitionException extends RuntimeException
 {
-    public DefinitionException() {
+    public DefinitionException()
+    {
     }
 
-    public DefinitionException(Throwable cause) {
+    public DefinitionException(Throwable cause)
+    {
         super(cause);
     }
 
-    public DefinitionException(String message) {
+    public DefinitionException(String message)
+    {
         super(message);
     }
 
-    public DefinitionException(String message, Throwable cause) {
+    public DefinitionException(String message, Throwable cause)
+    {
         super(message, cause);
     }
 
-    public DefinitionException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
