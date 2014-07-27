a75cb9aad75a5ee903e48da28d45aa07ee21059d
Romain Manni-Bucau <rmannibucau@apache.org>
2014-07-27 14:31:38 +0000

    jta 1.2 spec jar, mainly a copy from 1.1 with Transactional, TransactionalException and TransactionScoped additions
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk/geronimo-jta_1.2_spec@1613798 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/javax/transaction/Transactional.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+//
+// This source code implements specifications defined by the Java
+// Community Process. In order to remain compliant with the specification
+// DO NOT add / change / or delete method signatures!
+//
+package javax.transaction;
+
+import javax.enterprise.util.Nonbinding;
+import javax.interceptor.InterceptorBinding;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Inherited
+@InterceptorBinding
+@Target({TYPE, METHOD})
+@Retention(RUNTIME)
+public @interface Transactional {
+    public enum TxType {
+        REQUIRED, REQUIRES_NEW, MANDATORY, SUPPORTS, NOT_SUPPORTED, NEVER
+    }
+
+    TxType value() default TxType.REQUIRED;
+
+    @Nonbinding
+    public Class[] rollbackOn() default {};
+
+    @Nonbinding
+    public Class[] dontRollbackOn() default {};
+}
