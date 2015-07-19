dee29b8ad0cffad2614301e9aa93ccea1e3d600f
Hendrik Saly <salyh@apache.org>
2015-07-19 19:19:48 +0000

    Createing a branch for JSR 374 (Jsonp 1.1)
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk@1691844 13f79535-47bb-0310-9956-ffa450edef68

---
+++ geronimo-json_1.1_spec/src/main/java/javax/json/stream/JsonParser.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package javax.json.stream;
+
+import java.io.Closeable;
+import java.math.BigDecimal;
+
+public interface JsonParser extends Closeable {
+    boolean hasNext();
+
+    Event next();
+
+    String getString();
+
+    boolean isIntegralNumber();
+
+    int getInt();
+
+    long getLong();
+
+    BigDecimal getBigDecimal();
+
+    JsonLocation getLocation();
+
+    @Override
+    void close();
+
+    enum Event {
+        START_ARRAY, START_OBJECT,
+        KEY_NAME,
+        VALUE_STRING, VALUE_NUMBER, VALUE_TRUE, VALUE_FALSE, VALUE_NULL,
+        END_OBJECT, END_ARRAY
+    }
+}
+
