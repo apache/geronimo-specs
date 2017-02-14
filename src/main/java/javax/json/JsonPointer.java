66afac3710b30dac72cb582ce35aaac89930d419
Mark Struberg <struberg@apache.org>
2017-02-14 12:54:48 +0000

    GERONIMO-6558 apply changes based on the spec tickets Reinhard and I created
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk/geronimo-json_1.1_spec@1782959 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/javax/json/JsonPointer.java
+++ src/main/java/javax/json/JsonPointer.java
@@ -21,17 +21,17 @@ package javax.json;
 /**
  * <p>This class is an immutable representation of a JSON Pointer as specified in
  * <a href="http://tools.ietf.org/html/rfc6901">RFC 6901</a>.
- * </p>
+ *
  *
  * <p>JSON Pointer is a string syntax for identifying a specific value
  * within a JavaScript Object Notation (JSON) document [RFC4627].
  * JSON Pointer is intended to be easily expressed in JSON string values
  * as well as Uniform Resource Identifier (URI) [RFC3986] fragment identifiers.
- * </p>
+ *
  * <p> The method {@link #getValue getValue()} returns the referenced value.
  * The methods {@link #add add()}, {@link #replace replace()},
  * and {@link #remove remove()} executes the operations specified in
- * <a href="http://tools.ietf.org/html/rfc6902">RFC 6902</a>. </p>
+ * <a href="http://tools.ietf.org/html/rfc6902">RFC 6902</a>.
  *
  * @since 1.1
  */
@@ -39,21 +39,23 @@ public interface JsonPointer {
 
     JsonValue getValue(JsonStructure target);
 
-    JsonStructure add(JsonStructure target, JsonValue value);
 
-    JsonStructure replace(JsonStructure target, JsonValue value);
+    /**
+     * Add or replace the value at the position referenced by this JsonPointer with
+     * the new value
+     * @param target structure in which the newValue should be added or replaced
+     * @param newValue the new value to set
+     * @param <T>
+     * @return the new structure after modifying the original JsonStrucure
+     */
+    <T extends JsonStructure> T add(T target, JsonValue newValue);
 
-    JsonStructure remove(JsonStructure target);
 
-    JsonObject add(JsonObject target, JsonValue value);
+    <T extends JsonStructure> T  remove(T target);
 
-    JsonArray add(JsonArray target, JsonValue value);
+    boolean containsValue(JsonStructure target);
 
-    JsonObject replace(JsonObject target, JsonValue value);
+    <T extends JsonStructure> T  replace(T target, JsonValue value);
 
-    JsonArray replace(JsonArray target, JsonValue value);
 
-    JsonObject remove(JsonObject target);
-
-    JsonArray remove(JsonArray target);
 }
