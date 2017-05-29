17f4d8688befc6c526787c843abf111766d886a4
Mark Struberg <struberg@apache.org>
2017-05-29 16:14:46 +0000

    GERONIMO-6553 add ReqeustContext Control
    Contributed by John Ament (johndament) - txs for the patch!
    
    
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk/geronimo-jcdi_2.0_spec@1796659 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/javax/enterprise/context/control/ActivateRequestContext.java
@@ -0,0 +1,47 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package javax.enterprise.context.control;
+
+
+import javax.interceptor.InterceptorBinding;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * The container provides a built in interceptor that may be used to annotate classes and methods to indicate
+ * that a request context should be activated when this method is invoked.
+ *
+ * The request context will be activated before the method is called, and deactivated when the method invocation is
+ * complete (regardless of any exceptions being thrown).  If the context is already active, it is ignored, neither
+ * activated nor deactivated.
+ *
+ */
+@InterceptorBinding
+@Target({METHOD, TYPE})
+@Retention(RUNTIME)
+@Documented
+public @interface ActivateRequestContext
+{
+}
