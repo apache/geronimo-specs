8ed27b244dd36e70688e9ab4f0a4e60562cebd08
Raymond Augé <rotty3000@apache.org>
2018-08-16 15:38:07 +0000

    GERONIMO-6615 Apply service loader mediator to key modules useful for microprofile: JAXRS 2.1
    Signed-off-by: Raymond Auge <rotty3000@apache.org>
    
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk@1838202 13f79535-47bb-0310-9956-ffa450edef68

--- geronimo-jaxrs_2.1_spec/src/main/java/javax/ws/rs/sse/SseFinder.java
+++ geronimo-jaxrs_2.1_spec/src/main/java/javax/ws/rs/sse/SseFinder.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,11 +26,8 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Properties;
+import java.util.ServiceLoader;
 import java.util.logging.Logger;
-
-import javax.ws.rs.client.ClientBuilder;
-
-import org.apache.geronimo.osgi.locator.ProviderLocator;
 
 final class SseFinder {
 
@@ -44,9 +41,8 @@ final class SseFinder {
         final ClassLoader classLoader = getContextClassLoader();
 
         try {
-            final Object delegate = ProviderLocator.getService(FACTORY_ID, ClientBuilder.class, classLoader);
-            if (delegate != null) {
-                return delegate;
+            for (SseEventSource.Builder builder : ServiceLoader.load(SseEventSource.Builder.class)) {
+                return builder;
             }
 
             InputStream is = null;
@@ -111,7 +107,7 @@ final class SseFinder {
 
     private static Object newInstance(final String className, final ClassLoader classLoader) throws ClassNotFoundException {
         try {
-            Class spiClass;
+            Class<?> spiClass;
             if (classLoader == null) {
                 spiClass = Class.forName(className);
             } else {
