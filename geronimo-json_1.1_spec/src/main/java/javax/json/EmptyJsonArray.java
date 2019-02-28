d24d6353b299552c57bb0581f5b817276be5b653
Mark Struberg <struberg@apache.org>
2019-02-28 09:47:29 +0000

    GERONIMO-6712 remove JsonProvider resolving for JsonValue class init
    The usage of the full JsonProvider might have created a deadlock during class init.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/trunk@1854511 13f79535-47bb-0310-9956-ffa450edef68

---
+++ geronimo-json_1.1_spec/src/main/java/javax/json/EmptyJsonArray.java
@@ -0,0 +1,98 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package javax.json;
+
+import java.io.Serializable;
+import java.util.AbstractList;
+import java.util.List;
+
+class EmptyJsonArray extends AbstractList<JsonValue> implements JsonArray, Serializable {
+    @Override
+    public JsonValue get(int i) {
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public JsonObject getJsonObject(int index) {
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public JsonArray getJsonArray(int index) {
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public JsonNumber getJsonNumber(int index) {
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public JsonString getJsonString(int index) {
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public <T extends JsonValue> List<T> getValuesAs(Class<T> clazz) {
+        return null;
+    }
+
+    @Override
+    public String getString(int index) {
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public String getString(int index, String defaultValue) {
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public int getInt(int index) {
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public int getInt(int index, int defaultValue) {
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public boolean getBoolean(int index) {
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public boolean getBoolean(int index, boolean defaultValue) {
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public boolean isNull(int index) {
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public ValueType getValueType() {
+        return ValueType.ARRAY;
+    }
+}
