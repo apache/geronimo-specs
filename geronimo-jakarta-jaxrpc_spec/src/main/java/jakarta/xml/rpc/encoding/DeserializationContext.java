de8c3794e61a4d58a7a545b29cc27cf2ac0df0c8
Mark Struberg <struberg@apache.org>
2019-05-07 17:25:06 +0000

    add jax-rpc in jakarta style
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/specs/branches/jakarta@1858877 13f79535-47bb-0310-9956-ffa450edef68

---
+++ geronimo-jakarta-jaxrpc_spec/src/main/java/jakarta/xml/rpc/encoding/DeserializationContext.java
@@ -0,0 +1,30 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package jakarta.xml.rpc.encoding;
+
+/**
+ * The jakarta.xml.rpc.encoding.DeserializationContext interface is implemented by the JAX-RPC runtime system in an XML
+ * processing mechanism specific manner. A deserializer uses this interface to access and maintain context information
+ * during the deserialization..
+ *
+ * @version $Rev$ $Date$
+ */
+public interface DeserializationContext {}
+
